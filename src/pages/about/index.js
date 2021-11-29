21375d9d491ad47c17fc20caa0c43d34af765837
Victor Lopes <viclopes2621@gmail.com>
2021-11-29 03:46:17 -0300

    about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -3,6 +3,12 @@ import './styles.scss'
 
 export default function About() {
   return(
-    <div>Sobre</div>
+    <div className="about-wrapper">
+      <h2>Sobre</h2>
+      <h3>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h3>
+      <img src="https://rapidmaterials.com/wp-content/uploads/catalog/product/2081_1.png" alt="about"/>
+      <p>Em atividade desde o final de 2017, o abrigo nasceu quando a protetora Gabriela Xavier junto com outra protetora da região de Tatuí se uniram por amor e respeito aos animais.</p>
+      <p>Em atividade desde o final de 2017, o abrigo nasceu quando a protetora Gabriela Xavier junto com outra protetora da região de Tatuí se uniram por amor e respeito aos animais.</p>
+    </div>
   )
 }
