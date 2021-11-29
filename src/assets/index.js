a142dd93e4c6072c692e44c493fe450d32fb2000
Victor Lopes <viclopes2621@gmail.com>
2021-11-29 03:25:57 -0300

    adds assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,19 @@
 import { ReactComponent as SearchIcon } from './SearchIcon.svg'
 import { ReactComponent as BurgerIcon } from './MenuBurger.svg'
 import { ReactComponent as ArrowRight } from './ArrowRight.svg'
+import { ReactComponent as Telephone } from './Tel.svg'
+import { ReactComponent as Email } from './Email.svg'
+import { ReactComponent as FacebookIcon } from './Facebook-Icon.svg'
+import { ReactComponent as LinkedinIcon } from './Linkedin-Icon.svg'
+import { ReactComponent as TwitterIcon } from './Twitter-Icon.svg'
 
 export {
   SearchIcon,
   BurgerIcon,
-  ArrowRight
+  ArrowRight,
+  Telephone,
+  Email,
+  FacebookIcon,
+  LinkedinIcon,
+  TwitterIcon
 }
